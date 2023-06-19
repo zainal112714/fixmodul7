0b573db85df1649c58f61cac8df6af2a9d2f7a9a
zainal112714 <zainalarifindoraemon@gmail.com>
2023-06-19 22:14:28 +0700

    sampai database seeder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    function index()
+    {
+        $pageTitle = 'Home';
+
+        return view('home', ['pageTitle' => $pageTitle]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+Route::get('profile', ProfileController::class)->name('profile');
